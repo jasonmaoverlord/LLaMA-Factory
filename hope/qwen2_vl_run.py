fcd204387a497feacb54d102e4bae3d4215a1ea6
majinjin <2420582240@qq.com>
2024-10-27 11:31:39 +0800

    add logic

---
+++ hope/qwen2_vl_run.py
@@ -0,0 +1,53 @@
+from PIL import Image
+import requests
+import torch
+from transformers import Qwen2VLForConditionalGeneration, AutoProcessor
+
+model_dir = '/home/pod/shared-nvme/models/Qwen/Qwen2-VL-7B-Instruct'
+# Load the model in half-precision on the available device(s)
+model = Qwen2VLForConditionalGeneration.from_pretrained(
+    model_dir, device_map="auto", torch_dtype=torch.bfloat16
+)
+
+# The default range for the number of visual tokens per image in the model is 4-16384. You can set min_pixels and max_pixels according to your needs, such as a token count range of 256-1280, to balance speed and memory usage.
+min_pixels = 256*28*28
+max_pixels = 1280*28*28
+processor = AutoProcessor.from_pretrained(model_dir, min_pixels=min_pixels, max_pixels=max_pixels)
+# processor = AutoProcessor.from_pretrained(model_dir)
+
+# Image
+url = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+conversation = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "image",
+            },
+            {"type": "text", "text": "Describe this image."},
+        ],
+    }
+]
+
+
+# Preprocess the inputs
+text_prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
+# Excepted output: '<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\n<|vision_start|><|image_pad|><|vision_end|>Describe this image.<|im_end|>\n<|im_start|>assistant\n'
+
+inputs = processor(
+    text=[text_prompt], images=[image], padding=True, return_tensors="pt"
+)
+inputs = inputs.to("cuda")
+
+# Inference: Generation of the output
+output_ids = model.generate(**inputs, max_new_tokens=128)
+generated_ids = [
+    output_ids[len(input_ids) :]
+    for input_ids, output_ids in zip(inputs.input_ids, output_ids)
+]
+output_text = processor.batch_decode(
+    generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
+)
+print(output_text)
